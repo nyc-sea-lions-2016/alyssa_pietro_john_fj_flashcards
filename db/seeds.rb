81f99edd833647ccb063c8f49a7af59da6d1ba1c
Alyssa Ransbury <email": null>
2016-03-25 17:18:11 -0400

    Add easy flashcards set

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require 'csv'
 
 
+
 countries_capitals_flashcards = '/seeds/countries_capitals_flashcards.csv'
 
 first_deck = Deck.create(name: "Countries and Capitals")
@@ -13,6 +14,7 @@ CSV.foreach(countries_capitals_flashcards, options) do |line|
 end
 
 
+
 country_abbrev_flashcards = '/seeds/country_abbrev_flashcards.csv'
 
 second_deck = Deck.create(name: "Countries and Abbreviations")
@@ -20,6 +22,19 @@ second_deck = Deck.create(name: "Countries and Abbreviations")
 options = {headers: true}
 deck_info_2 = {deck_id: second_deck.id}
 
-CSV.foreach(countries_capitals_flashcards, options) do |line|
+CSV.foreach(country_abbrev_flashcards, options) do |line|
   Card.create(line.merge(deck_info_2))
+end
+
+
+
+easy_flashcards = '/seeds/easy_flashcards.csv'
+
+third_deck = Deck.create(name: "Repeat After Me")
+
+options = {headers: true}
+deck_info_3 = {deck_id: third_deck.id}
+
+CSV.foreach(easy_flashcards, options) do |line|
+  Card.create(line.merge(deck_info_3))
 end
