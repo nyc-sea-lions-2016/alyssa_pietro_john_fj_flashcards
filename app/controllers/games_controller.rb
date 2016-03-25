dc59b5ea0c3cfe7d366a93cb7fde838ea6245e09
Pietro-Martini <email": null>
2016-03-25 19:21:06 -0400

    add part of the games controller logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,12 +4,16 @@ end
 
 post '/games/:id' do
   @game = Game.find_by(id: params[:id])
-  @cards = @game.deck.cards
-  @random_card = @cards.sample
-  @cards.delete(@random_card)
+  @cards = Card.cards_in_game(@game.id)
   if @cards.length > 0
+    @random_card = @cards.sample
+    @cards.delete(@random_card)
     redirect 'games/:id'
   else
-
+    #logic for game over
+    redirect 'games/:id'
   end
 end
+
+#stuff
+sessions[:game_id] = []
--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,9 @@ class Card < ActiveRecord::Base
   belongs_to :deck
   has_many :guesses
   validates :prompt, :answer, presence: true
+
+  def self.cards_in_game(game_id)
+    Card.where(game_id: game_id)
+  end
+
 end
