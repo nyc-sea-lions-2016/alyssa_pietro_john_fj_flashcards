03b5ba4d50da24498e75e67ce6d14d65a979eaad
Pietro-Martini <email": null>
2016-03-25 16:04:17 -0400

    add indexes to migrations

--- db/migrate/20160325133645_create_cards.rb
+++ db/migrate/20160325133645_create_cards.rb
@@ -1,7 +1,7 @@
 class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
-      t.references :deck
+      t.references :deck, index: true 
       t.string :prompt, null: false
       t.string :answer, null: false
 
--- db/migrate/20160325133704_create_guesses.rb
+++ db/migrate/20160325133704_create_guesses.rb
@@ -1,9 +1,9 @@
 class CreateGuesses < ActiveRecord::Migration
   def change
     create_table :guesses do |t|
-      t.references :card
-      t.references :game
-      t.string :guess, null: false 
+      t.references :card, index: true
+      t.references :game, index: true 
+      t.string :guess, null: false
 
       t.timestamps null: false
     end
