101448be339ffdc063e3ef332350b98cd1ae926e
Pietro-Martini <email": null>
2016-03-25 18:23:27 -0400

    change models

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,5 +1,4 @@
 class Deck < ActiveRecord::Base
   has_many :cards
-  belongs_to :game
   validates :name, presence: true, uniqueness: true
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,4 @@
 class Game < ActiveRecord::Base
-  belongs_to :deck
   belongs_to :user
   has_many :guesses
   validates :name, presence: true
