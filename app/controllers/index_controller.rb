e75f5923957d820a9bb0c4ee9e496054a29baa0e
FJunior225 <fcollins@live.com>
2016-03-25 17:05:52 -0400

    Add post form for new game for each deck with controller method included

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,3 +2,15 @@ get '/flashcards' do
   @decks = Deck.all
   erb :'/flashcards/index'
 end
+
+post '/games' do
+  @deck = Deck.new
+  @deck.id = params[:game_info][:deck_id]
+  if logged_in?
+    @game = Game.create(deck_id, user_id)
+  else
+    @game = Game.create(deck_id, user_id = 0)
+  end
+  redirect "games/#{@game.id}"
+end
+
