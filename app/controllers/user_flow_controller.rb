4d2c433a1627b65e05df0fe0681220b7b7fe51c6
Alyssa Ransbury <email": null>
2016-03-25 15:29:53 -0400

    Add flows for login and user creation

--- app/controllers/user_flow_controller.rb
+++ app/controllers/user_flow_controller.rb
@@ -1,21 +1,34 @@
 
 # Login
 get '/sessions/new' do
-
-
+ erb :"sessions/new"
 end
 
 
 post '/sessions/new' do
-
+  found_user = User.find_by(username: params[:user][:username])
+  if found_user && found_user.authenticate(params[:user][:password])
+    session[:user_session_id] = found_user.id
+    redirect '/'
+  else
+    @errors = ["Incorrect username or password"]
+    erb :"sessions/new"
+  end
 end
 
 
 #Create user
 get '/users/new' do
-
+  erb :"users/new"
 end
 
 post '/users/new' do
-
+  @new_user = User.new(params[:user])
+  if @new_user.save
+    session[:user_session_id] = @new_user.id
+    redirect '/'
+  else
+    @errors = @new_user.errors.full_messages
+    erb :"users/new"
+  end
 end
