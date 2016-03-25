b37df874a715ad5dcd3de266110419a927dc79d4
Pietro-Martini <email": null>
2016-03-25 17:05:48 -0400

    add has many guesses to game model

--- db/migrate/20160325141436_create_user_table.rb
+++ db/migrate/20160325141436_create_user_table.rb
@@ -5,7 +5,7 @@ class CreateUserTable < ActiveRecord::Migration
       t.string :email, null: false
       t.string :password_digest, null: false
 
-      t.timestamps, null: false
+      t.timestamps null: false
     end
   end
 end
