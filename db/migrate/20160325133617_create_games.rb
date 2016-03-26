c9d591216d122a2bce636dbedb3ff9c90f78edb0
Alyssa Ransbury <email": null>
2016-03-26 18:15:15 -0400

    Add correct_on_first_guess column to games table

--- db/migrate/20160325133617_create_games.rb
+++ db/migrate/20160325133617_create_games.rb
@@ -2,7 +2,8 @@ class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
       t.references :deck, index: true
-      t.references :user, index: true 
+      t.references :user, index: true
+      t.integer :correct_on_first_guess
 
       t.timestamps null: false
     end
