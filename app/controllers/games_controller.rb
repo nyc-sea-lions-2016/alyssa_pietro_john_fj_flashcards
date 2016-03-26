c3287de29888484fde0f9d270281a29e85602ed2
Alyssa Ransbury <email": null>
2016-03-26 13:27:46 -0400

    Edit game logic and update game view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,19 +1,23 @@
 get '/games/:id' do
+  binding.pry
+  @game = Game.find_by(id: params[:id])
+  @deck = Deck.find_by(id: @game.deck_id)
+  @cards = Card.cards_in_game(@game.id)
+  response.set_cookie("game_cards_#{@game.id}", :value => @cards)
   erb :'games/show'
 end
 
-# post '/games/:id' do
-#   @game = Game.find_by(id: params[:id])
-#   @cards = Card.cards_in_game(@game.id)
-#   if @cards.length > 0
-#     @random_card = @cards.sample
-#     @cards.delete(@random_card)
-#     redirect 'games/:id'
-#   else
-#     #logic for game over
-#     redirect 'games/:id'
-#   end
-# end
-
-#stuff
-# sessions[:game_id] = []
+post '/games/:id' do
+  # @game = Game.find_by(id: params[:id])
+  # @cards = Card.cards_in_game(@game.id)
+  @game_id = params[:id]
+  request.cookies["game_cards_#{@game_id}"]
+  if @cards.length > 0
+    @random_card = @cards.sample
+    @cards.delete(@random_card)
+    redirect 'games/:id'
+  else
+    #logic for game over
+    redirect 'games/:id'
+  end
+end
