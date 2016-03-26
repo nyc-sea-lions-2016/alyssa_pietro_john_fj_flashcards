ee63a430f24c70f3f1fdd8327809028ec766926e
Pietro-Martini <email": null>
2016-03-26 19:29:53 -0400

    added create deck feature with full error messages

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,7 +1,14 @@
 get '/decks/new' do
-
+  erb :"decks/new"
 end
 
-post '/decks' do
-
-end 
+post '/decks/new' do
+  @deck = Deck.new(params[:deck])
+  @card = Card.new(params[:card])
+  if @deck.save
+    redirect '/'
+  else
+    @errors = @deck.errors.full_messages
+    erb :'/decks/new'
+  end
+end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -16,7 +16,6 @@ get '/games/:id' do
     @cards = Card.cards_in_game(@deck.id)
     @card_ids = Card.card_ids_string(@cards)
     response.set_cookie("game_cards_#{@game.id}", :value => @card_ids)
-    binding.pry
     @random_card = @cards.sample
   end
 
@@ -51,4 +50,3 @@ post '/games/:id' do
 
   redirect 'games/:id'
 end
-
