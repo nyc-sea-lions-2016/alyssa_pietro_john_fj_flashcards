a71f28a524f8dc69e4c1d8648621e404e6a7d1df
Alyssa Ransbury <email": null>
2016-03-26 16:14:32 -0400

    Add additional controller logic for game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,7 +10,8 @@ get '/games/:id' do
     response.set_cookie("game_cards_#{@game.id}", :value => @cards)
   elsif @cards && @cards.length == 0
     @game_over = true
-    #bring this back into view
+    @game.correct_on_first_guess = request.cookies["correct_cards_#{@game.id}"].length
+    #confirm that this is the name in the database (above)
   else
     @cards = Card.cards_in_game(@game.id)
     response.set_cookie("game_cards_#{@game.id}", :value => @cards)
@@ -22,13 +23,29 @@ end
 post '/games/:id' do
   @user_response = params[:game_info][:response]
   @game_id = params[:id]
-  @cards = request.cookies["game_cards_#{@game_id}"]
+  @cards = request.cookies["correct_cards_#{@game.id}"]
+  correct_cards = request.cookies["correct_cards_#{@game.id}"]
   @current_card = params[:game_info][:current_card]
+  correct = Card.correct_answer?(@current_card.id, @user_response)
 
-  if Card.correct_answer?(@current_card.id, @user_response)
+  unless correct_cards
+    correct_cards = response.set_cookie("correct_cards_#{@game.id}", :value => [])
+  end
 
+  user_guess = Guess.create(card_id: @current_card.id, game_id: @game_id, entry: @user_response)
+
+  if correct && Guess.first_guess?(@game_id, @current_card.id)
+
+    # add card obj to correct cards hash
+    # delete card from @cards cookie
+    response.set_cookie("correct_cards_#{@game.id}", :value => [])
+  elsif correct
+    # delete card from @cards cookie
+  else
+    # do nothing and redirect
   end
 
 
   redirect 'games/:id'
 end
+
--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,4 +2,9 @@ class Guess < ActiveRecord::Base
   belongs_to :card
   belongs_to :game
   validates :entry, presence: true
+
+  def first_guess?(game_id, card_id)
+    card_guesses = Guess.where(game_id: game_id, card_id: card_id)
+    card_guesses.length == 1
+  end
 end
