0ca373e6825e9dbbc3d2d5eec59a42e31edf2608
Pietro-Martini <email": null>
2016-03-26 15:57:35 -0400

    add deck_name method to deck model

--- db/migrate/20160325133632_create_decks.rb
+++ db/migrate/20160325133632_create_decks.rb
@@ -1,7 +1,6 @@
 class CreateDecks < ActiveRecord::Migration
   def change
     create_table :decks do |t|
-        t.references :game 
         t.string :name, null: false
 
         t.timestamps null: false
